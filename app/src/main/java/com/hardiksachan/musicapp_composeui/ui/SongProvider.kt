0cc69b12066cad713217122fa51aafd026b863ea
Hardik Sachan <sachanhardik@gmail.com>
2021-09-20 03:38:09 +0530

    Home Screen UI done

---
+++ app/src/main/java/com/hardiksachan/musicapp_composeui/ui/SongProvider.kt
@@ -0,0 +1,60 @@
+package com.hardiksachan.musicapp_composeui.ui
+
+import com.hardiksachan.musicapp_composeui.R
+import com.hardiksachan.musicapp_composeui.ui.model.Song
+
+object SongProvider {
+    val itsYou = Song(
+        "it's you",
+        "Ali Gatie",
+        R.drawable.ali_gatie__its_you
+    )
+
+    val beAlright = Song(
+        "be alright",
+        "Dean Lewis",
+        R.drawable.dean_lewis__be_alright
+    )
+
+    val arcade = Song(
+        "arcade",
+        "Duncan Laurence",
+        R.drawable.duncan_laurence__arcade
+    )
+
+    val loveMeLikeYouDo = Song(
+        "love me like you do",
+        "Ellie Goulding",
+        R.drawable.ellie_goulding__love_me_like_you_do
+    )
+
+    val driversLicense = Song(
+        "drivers license",
+        "Olivia Rodrigo",
+        R.drawable.olivia_rodrigo__drivers_license
+    )
+
+    val loseYouToLoveMe = Song(
+        "lose you to love me",
+        "Selena Gomez",
+        R.drawable.selena_gomez__lose_you_to_love_me
+    )
+
+    val youBrokeMeFirst = Song(
+        "you broke me first",
+        "Tate McRae",
+        R.drawable.tate_mcrae__you_broke_me_first
+    )
+
+    val allTooWell = Song(
+        "all too well",
+        "Taylor Swift",
+        R.drawable.taylor_swift__all_too_well
+    )
+
+    val loveStory = Song(
+        "love story",
+        "Taylor Swift",
+        R.drawable.taylor_swift__love_story
+    )
+}
--- app/src/main/java/com/hardiksachan/musicapp_composeui/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/hardiksachan/musicapp_composeui/ui/screens/HomeScreen.kt
@@ -1,7 +1,11 @@
 package com.hardiksachan.musicapp_composeui.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -9,11 +13,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hardiksachan.musicapp_composeui.R
+import com.hardiksachan.musicapp_composeui.ui.SongProvider
+import com.hardiksachan.musicapp_composeui.ui.model.Song
 
 @Composable
 fun HomeScreen(handleThemeSwitch: () -> Unit) {
@@ -27,7 +34,35 @@ fun HomeScreen(handleThemeSwitch: () -> Unit) {
             ) { handleThemeSwitch() }
         }
     ) {
-
+        LazyColumn(
+            modifier = Modifier
+                .padding(start = 32.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start,
+        ) {
+            itemsIndexed(
+                items = listOf(
+                    "recent activity" to listOf(
+                        SongProvider.allTooWell,
+                        SongProvider.beAlright,
+                        SongProvider.loveStory
+                    ),
+                    "trending tracks" to listOf(
+                        SongProvider.itsYou,
+                        SongProvider.driversLicense,
+                        SongProvider.loveMeLikeYouDo
+                    ),
+                    "recommended" to listOf(
+                        SongProvider.loseYouToLoveMe,
+                        SongProvider.arcade,
+                        SongProvider.youBrokeMeFirst
+                    )
+                )
+            ) { _, sectionPair ->
+                SongsRow(title = sectionPair.first, songs = sectionPair.second)
+            }
+        }
     }
 }
 
@@ -64,6 +99,78 @@ fun GreetingSection(
                     handleThemeSwitch()
                 },
         )
+    }
+}
 
+@Composable
+fun SongsRow(
+    title: String,
+    songs: List<Song>
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 32.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(end = 32.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.h5,
+            )
+            Icon(
+                painter = painterResource(R.drawable.ic_right_arrow),
+                contentDescription = "right arrow",
+                modifier = Modifier
+                    .size(28.dp)
+                    .padding(bottom = 8.dp)
+            )
+        }
+
+        LazyRow(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            itemsIndexed(songs) { _, song ->
+                SongCard(song = song, modifier = Modifier.padding(end = 16.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun SongCard(
+    song: Song,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(id = song.image),
+            contentDescription = song.name,
+            modifier = Modifier
+                .size(125.dp)
+                .clip(MaterialTheme.shapes.large),
+        )
+        Spacer(modifier = Modifier.padding(vertical = 2.dp))
+        Text(
+            text = song.name,
+            style = MaterialTheme.typography.body1,
+                fontWeight = FontWeight.W600,
+            letterSpacing = (-0.5).sp
+
+        )
+
+        Spacer(modifier = Modifier.padding(vertical = 1.dp))
+        Text(text = song.artist, style = MaterialTheme.typography.body2)
     }
 }
